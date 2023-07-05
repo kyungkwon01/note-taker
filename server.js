2a7aea2a554e459f7272d32f6744ddd2bdeb1b83
Kyung Kwon <kyungkwon01@gmail.com>
2023-07-05 11:20:39 -0600

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,12 +9,63 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('Develop/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
 	res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+	res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', function (req, res) {
+	fs.readFile('db/db.json', 'utf8', (err, data) => {
+		var jsonData = JSON.parse(data);
+		console.log(jsonData);
+		res.json(jsonData);
+	});
+});
+
+const readThenAppendToJson = (content, file) => {
+	fs.readFile(file, 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			const parsedData = JSON.parse(data);
+			parsedData.push(content);
+			writeNewNoteToJson(file, parsedData);
+		}
+	});
+};
+
+const writeNewNoteToJson = (destination, content) =>
+	fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+		err ? console.error(err) : console.info(`\nData written to ${destination}`)
+	);
+
+app.post('/api/notes', (req, res) => {
+	const { title, text } = req.body;
+	if (title && text) {
+		const newNote = {
+			title: title,
+			text: text,
+			id: uniqid(),
+		};
+
+		readThenAppendToJson(newNote, 'db/db.json');
+
+		const response = {
+			status: 'success',
+			body: newNote,
+		};
+
+		res.json(response);
+	} else {
+		res.json('Error in posting new note');
+	}
+});
+
 app.listen(PORT, () => {
-	console.log(`Example app listening at http://localhost:${PORT}`);
+	console.log(`App listening at http://localhost:${PORT}`);
 });
